6998d667b17c62ff6120f95bc2807dca39d0a12e
ankitsensei <main.ankitbhagat@gmail.com>
2024-07-12 17:05:09 +0530

    can't fix table code

--- demo/src/Table.jsx
+++ demo/src/Table.jsx
@@ -1,18 +1,44 @@
 import React, { useState } from 'react';
 
 function table() {
-    const [start, setStart] = useState('06:00');
-    const [end, setEnd] = useState('07:00');
-    const [task, setTask] = useState(['wake up']);
+    const [start, setStart] = useState('');
+    const [end, setEnd] = useState('');
+    const [task, setTask] = useState(['']);
+    const data = [
+        ['hello', 'hello1', 'hello2'],
+        ['gang', 'gang1', 'gang2'],
+    ];
     return (
-        <div>
-            <tbody>
-                <td>
-                    {start.map((element, index) => {
-                        return element.index;
-                    })}
-                </td>
-            </tbody>
+        <div className='flex flex-col items-center py-20'>
+            <div className='flex gap-2 text'>
+                <div>
+                    <input
+                        className='text-zinc-950 px-4 py-2 w-36 rounded-md outline-none'
+                        type='time'
+                        placeholder='Start at'
+                    />
+                </div>
+                <div>
+                    <input
+                        className='text-zinc-950 px-4 py-2 w-36- rounded-md outline-none'
+                        type='time'
+                        placeholder='End at'
+                    />
+                </div>
+                <div>
+                    <input
+                        className='text-zinc-950 px-4 py-2 w-96 rounded-md outline-none'
+                        type='text'
+                        placeholder='Enter your Task'
+                    />
+                </div>
+                <button className='bg-green-600 w-24 rounded-md text-3xl hover:bg-green-700 active:bg-green-800'>
+                    +
+                </button>
+            </div>
+            <div>
+                <p>asd</p>
+            </div>
         </div>
     );
 }
