f1c76153e2592b1a8458a9599c39404008d422ac
ankitsensei <main.ankitbhagat@gmail.com>
2024-07-30 15:52:24 +0530

    added a nike logo to the dir

--- brand-page-nike/src/App.jsx
+++ brand-page-nike/src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-    return <div className='bg-zinc-900 text-white w-full h-screen'>App</div>;
+    return (
+        <div className='bg-zinc-900 text-white w-full h-screen'>
+            <h1 className='text-3xl font-bold'>Hello Bose!!</h1>
+        </div>
+    );
 }
 
 export default App;
