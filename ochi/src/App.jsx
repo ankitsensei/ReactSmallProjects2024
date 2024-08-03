a948946773649bd7b16bfdbb5970a90253838524
ankitsensei <main.ankitbhagat@gmail.com>
2024-08-03 22:46:00 +0530

    1st page and little bit of navbar

--- ochi/src/App.jsx
+++ ochi/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
 import Navbar from './components/navbar.jsx';
+
 function App() {
     return (
         <div className='bg-zinc-900 text-white w-full h-screen'>
-            <Navbar />
+            <div className='page1'>
+                <Navbar />
+                <div className='text-9xl tracking-tighter font-semibold mt-32 ml-16'>
+                    <div className='flex items-center'>
+                        <div className='w-40 h-24 bg-rose-600 rounded-lg'></div>
+                        <h1>LET'S START</h1>
+                    </div>
+                    <h1>A PROJECT TOGETHER</h1>
+                </div>
+            </div>
         </div>
     );
 }
--- ochi/src/components/navbar.jsx
+++ ochi/src/components/navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function navbar() {
     return (
         <div className='w-full px-20 py-8 flex justify-between'>
-            <div className='logo'>ochi</div>
+            <div className='logo text-2xl font-semibold'>ochi</div>
             <div className='flex text-md font-semibold gap-96'>
                 <div className='links flex gap-8'>
                     {['Services', 'Our Work', 'About Us', 'Insights'].map(
