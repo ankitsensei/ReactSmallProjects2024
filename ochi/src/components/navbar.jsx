c1dfac432ebb915b245b21a2cfad21aaf717248d
ankitsensei <main.ankitbhagat@gmail.com>
2024-08-03 22:20:14 +0530

    navbar

--- ochi/src/components/navbar.jsx
+++ ochi/src/components/navbar.jsx
@@ -4,14 +4,16 @@ function navbar() {
     return (
         <div className='w-full px-20 py-8 flex justify-between'>
             <div className='logo'>ochi</div>
-            <div className='links flex gap-8'>
-                {['Services', 'Our Work', 'About Us', 'Insights'].map(
-                    (item, index) => (
-                        <a key={index} className='text-md font-semibold '>
-                            {item}
-                        </a>
-                    )
-                )}
+            <div className='flex text-md font-semibold gap-96'>
+                <div className='links flex gap-8'>
+                    {['Services', 'Our Work', 'About Us', 'Insights'].map(
+                        (item, index) => (
+                            <a key={index}>{item}</a>
+                        )
+                    )}
+                </div>
+
+                <div className='contactus'>Contact Us</div>
             </div>
         </div>
     );
