1ea02e2bfabbf9a6aeae8765122f34593e8d966c
ankitsensei <main.ankitbhagat@gmail.com>
2024-08-07 17:16:23 +0530

    now tasks can be added to the list

--- TodoApp/src/App.jsx
+++ TodoApp/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
         'english assign',
         'chemistry assign',
     ]);
+
     return (
         <div className='flex flex-col items-center pt-20 w-full h-screen text-2xl bg-zinc-900 text-white'>
             <h1 className='text-5xl font-semibold '>TODO APP</h1>
@@ -17,7 +18,18 @@ function App() {
                     placeholder='Add a task'
                     className='w-80 px-4 py-2 outline-none rounded bg-zinc-900 border-2 border-zinc-100'
                 />
-                <button className='ml-2 px-4 py-2 rounded border-2 border-green-600 text-green-600'>
+                <button
+                    className='ml-2 px-4 py-2 rounded border-2 border-green-600 text-green-600'
+                    onClick={() => {
+                        if (document.querySelector('input').value) {
+                            setItems([
+                                ...items,
+                                document.querySelector('input').value,
+                            ]);
+                            document.querySelector('input').value = '';
+                        }
+                    }}
+                >
                     Add
                 </button>
             </div>
