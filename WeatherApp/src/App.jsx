916d77a7db619686e92185f317c58e9fea48e0c4
ankitsensei <main.ankitbhagat@gmail.com>
2024-08-08 10:59:53 +0530

    search bar fronted done

--- WeatherApp/src/App.jsx
+++ WeatherApp/src/App.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { IoSearchCircle } from 'react-icons/io5';
 
 function App() {
-    return <div>App</div>;
+    return (
+        <div className='bg-zinc-900 text-white w-full h-screen'>
+            <div className='flex flex-col items-center pt-20 h-full'>
+                <h1 className='text-5xl font-bold'>Weather App</h1>
+                <div className='border-2 border-zinc-600 mt-10 w-96 h-[600px] rounded-xl p-5 bg-summerImg bg-cover'>
+                    <div className='flex items-center justify-center'>
+                        <input
+                            type='text'
+                            className='w-64 h-10 rounded-md bg-zinc-800 bg-opacity-70 border-2 border-zinc-50 px-2 font-semibold outline-none'
+                        />
+                        <button>
+                            <IoSearchCircle className='w-14 h-14 ' />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
