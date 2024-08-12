87b7ed7d9c2d1c7305e251896476a1df074740ab
ankitsensei <main.ankitbhagat@gmail.com>
2024-08-12 08:50:54 +0530

    trying to jump into another page

--- DiceGame/src/components/Dice.jsx
+++ DiceGame/src/components/Dice.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Btn from './Btn';
+import { Link } from 'react-router-dom';
+
 function Dice() {
     return (
         <div className='flex justify-center items-center text-3xl px-24 w-full h-screen'>
@@ -10,7 +12,9 @@ function Dice() {
             />
             <div className='flex flex-col items-end gap-5'>
                 <h1 className='text-9xl font-bold'>DICE GAME</h1>
-                <Btn text='Play Now' />
+                <Link to='./DicePlay'>
+                    <Btn text='Play Now' />
+                </Link>
             </div>
         </div>
     );
---
+++ DiceGame/src/components/DicePlay.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DicePlay() {
+    return (
+        <div>
+            <h1 className='text-3xl'>This is DicePlay page</h1>
+        </div>
+    );
+}
+
+export default DicePlay;
