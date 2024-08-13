21847bc7a7b3690b6240d3f743134d38946733a3
ankitsensei <main.ankitbhagat@gmail.com>
2024-08-13 08:29:37 +0530

    added back button and  numbers of buttons for game

--- DiceGame/src/DicePlay.jsx
+++ DiceGame/src/DicePlay.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Dice from './Dice';
+import Btn2 from './components/Btn2';
+import { IoMdArrowRoundBack } from 'react-icons/io';
 
 function DicePlay() {
     return (
         <div>
-            <h1 className='text-3xl'>This is DicePlay page</h1>
+            <div className='flex justify-between items-center px-10 py-5'>
+                <div className='flex items-center justify-center'>
+                    <Link to='/'>
+                        <button className='bg-zinc-900 text-white px-4 py-[10px] rounded-md'>
+                            <IoMdArrowRoundBack />
+                        </button>
+                    </Link>
+
+                    <h1 className='text-5xl font-bold'>DICE GAME</h1>
+                </div>
+                <ul className='flex gap-2'>
+                    <li>
+                        <Btn2 text='1' />
+                    </li>
+                    <li>
+                        <Btn2 text='2' />
+                    </li>
+                    <li>
+                        <Btn2 text='3' />
+                    </li>
+                    <li>
+                        <Btn2 text='4' />
+                    </li>
+                    <li>
+                        <Btn2 text='5' />
+                    </li>
+                    <li>
+                        <Btn2 text='6' />
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
---
+++ DiceGame/src/components/Btn2.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Btn({ text, className = className }) {
+    return (
+        <div>
+            <button className={className}>{text}</button>
+        </div>
+    );
+}
+
+Btn.defaultProps = {
+    className: 'bg-zinc-900 text-white px-4 py-2 rounded-md w-16 h-15 text-lg',
+};
+
+export default Btn;
