2686822fac3bddd724b9b60522ade7ce751a00fe
ankitsensei <main.ankitbhagat@gmail.com>
2024-10-29 14:04:10 +0530

    added few questions to  Data file... now data is showing in page

---
+++ QuizApp/src/App.tsx
@@ -0,0 +1,23 @@
+import data from './Data.js'
+
+const App = () => {
+  return (
+    <div className="bg-zinc-900 text-zinc-200 flex flex-col px-[500px]">
+      <h1 className = "text-5xl flex flex-col justify-center items-center h-20">Quiz Questions</h1>
+      <div className='flex flex-col gap-10'>
+        {
+          data.map((item, index) => (
+            <li className='list-decimal flex flex-col font-semibold text-lg' key={index}>{item.question} <ul>
+              {
+                item.option.map((opt, idx) => (
+                  <li className='list-disc font-normal text-sm' key={idx}>{opt}</li>
+                ))
+}</ul></li>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ QuizApp/src/Data.js
@@ -0,0 +1,39 @@
+const data = [
+    {
+        question: "What is the value of 12*8?",
+        option: [80, 96, 100, 108],
+        answer: 96
+    },
+    {
+        question: "What planet is known as the 'Red Planet'?",
+        option: ["Earth", "Venus", "Mars", "Jupiter"],
+        answer: "Mars"
+    },
+    {
+        question: "Which is the longest river in the world?",
+        option: ["Amazon", "Nile", "Yangtze", "Mississippi"],
+        answer: "Nile"
+    },
+    {
+        question: "Choose the correct form: 'She _____ the book yesterday.'",
+        option: ["read", "reads", "is reading", "reading"],
+        answer: "read"
+    },
+    {
+        question: "Who was the first President of the United States?",
+        option: ["Abraham Lincoln", "Thomas Jefferson", "George Washington", "John Adams"],
+        answer: "George Washington"
+    },
+    {
+        question: "What is the powerhouse of the cell?",
+        option: ["Nucleus", "Mitochondria", "Ribosome", "Chloroplast"],
+        answer: "Mitochondria"
+    },
+    {
+        question: "What is the unit of Force in the International System of Units (SI)?",
+        option: ["Joule", "Newton", "Pascal", "Watt"],
+        answer: "Newton"
+    }
+];
+
+export default data;
