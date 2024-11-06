e2bd24ba239936bda45af397ab01aef5dd724c32
ankitsensei <main.ankitbhagat@gmail.com>
2024-11-06 22:49:56 +0530

    Academics section yet to be added fully

--- form/src/App.tsx
+++ form/src/App.tsx
@@ -56,6 +56,49 @@ const App = () => {
               <input type="number" placeholder='20' className='outline-none w-full' />
             </div>
           </div>
+          <div className='mt-5'>
+            <p className='text-lg font-semibold text-blue-600 flex items-center gap-4'>Academics</p>
+            <div className='flex gap-2 mt-1'>
+              <div className='w-80 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
+                <p>University:</p>
+                <input type="text" placeholder='Jharkhand Rai University' className='outline-none w-full' />
+              </div>
+              <div className='w-72 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
+                <p>Stream:</p>
+                <select className="w-full outline-none">
+                  <option>Engineering</option>
+                  <option>Management</option>
+                  <option>Science</option>
+                  <option>Arts</option>
+                  <option>Commerce</option>
+                  <option>Computer Application</option>
+                  <option>Education</option>
+                  <option>Medical</option>
+                  <option>Pharmacy</option>
+                  <option>Paramedical</option>
+                  <option>Law</option>
+                  <option>Mass Communication</option>
+                  <option>Design</option>
+                  <option>Vocational Courses</option>
+                  <option>Hotel Management</option>
+                  <option>Agriculture</option>
+                  <option>Architecture</option>
+                </select>
+              </div>
+              <div className='w-80 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
+                <p>Course:</p>
+                <select className="w-full outline-none">
+                  <option>BE/B.Tech</option>
+                  <option>ME/M.Tech</option>
+                  <option>Polytechnic</option>
+                  <option>BE/B.Tech Lateral</option>
+                  <option>M.Phil/Ph.D in Engineering</option>
+                  <option>Diploma in Engineering</option>
+                  <option>Training Programs</option>
+                </select>
+              </div>  
+            </div>
+          </div>
         </div>
       </div>
     </div>
