e6de03a66b644a6e68d6992ae7b55bcc0f15d6c1
ankitsensei <main.ankitbhagat@gmail.com>
2024-11-07 12:12:25 +0530

    academics ui done

--- form/src/App.tsx
+++ form/src/App.tsx
@@ -12,7 +12,7 @@ const App = () => {
             <h1 className='text-2xl font-semibold text-zinc-600 flex items-center gap-4'>New audience <FontAwesomeIcon className='text-zinc-600 text-lg' icon={faWandMagicSparkles} /></h1>
             <button className='bg-blue-600 text-white w-28 h-8 rounded-full font-semibold outline-none'>Save</button>
           </div>
-          <div className='flex gap-2 mt-20'>
+          <div className='flex flex-wrap gap-2 mt-20 '>
             <div className='w-64 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
               <p>Name: </p>
               <input type="text" placeholder='John Doe  ' className='outline-none w-full' />
@@ -48,7 +48,6 @@ const App = () => {
                 <option>Androgyne</option>
                 <option>Other</option>
                 <option>Prefer not to say</option>
-
               </select>
             </div>
             <div className='w-40 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
@@ -56,47 +55,25 @@ const App = () => {
               <input type="number" placeholder='20' className='outline-none w-full' />
             </div>
           </div>
-          <div className='mt-5'>
+          <div className='mt-5 flex flex-col'>
             <p className='text-lg font-semibold text-blue-600 flex items-center gap-4'>Academics</p>
-            <div className='flex gap-2 mt-1'>
+            <div className='flex flex-wrap gap-2 mt-1'>
               <div className='w-80 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
                 <p>University:</p>
                 <input type="text" placeholder='Jharkhand Rai University' className='outline-none w-full' />
               </div>
               <div className='w-72 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
                 <p>Stream:</p>
-                <select className="w-full outline-none">
-                  <option>Engineering</option>
-                  <option>Management</option>
-                  <option>Science</option>
-                  <option>Arts</option>
-                  <option>Commerce</option>
-                  <option>Computer Application</option>
-                  <option>Education</option>
-                  <option>Medical</option>
-                  <option>Pharmacy</option>
-                  <option>Paramedical</option>
-                  <option>Law</option>
-                  <option>Mass Communication</option>
-                  <option>Design</option>
-                  <option>Vocational Courses</option>
-                  <option>Hotel Management</option>
-                  <option>Agriculture</option>
-                  <option>Architecture</option>
-                </select>
+                <input type="text" placeholder='Computer Science' className='outline-none w-full' />
               </div>
               <div className='w-80 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
                 <p>Course:</p>
-                <select className="w-full outline-none">
-                  <option>BE/B.Tech</option>
-                  <option>ME/M.Tech</option>
-                  <option>Polytechnic</option>
-                  <option>BE/B.Tech Lateral</option>
-                  <option>M.Phil/Ph.D in Engineering</option>
-                  <option>Diploma in Engineering</option>
-                  <option>Training Programs</option>
-                </select>
+                <input type="text" placeholder='B.Tech' className='outline-none w-full' />
               </div>  
+              <div className='w-44 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
+                <p>Semester:</p>
+                <input type="number" placeholder='3' className='outline-none w-full' />
+              </div> 
             </div>
           </div>
         </div>
