7708b241222ea2d283b16309d2a1db886710ce4e
ankitsensei <main.ankitbhagat@gmail.com>
2024-11-07 12:55:10 +0530

    back to normal white bg

--- form/src/App.tsx
+++ form/src/App.tsx
@@ -4,31 +4,31 @@ import { faWandMagicSparkles } from '@fortawesome/free-solid-svg-icons'
 import Menu from './components/Menu'
 const App = () => {
   return (
-    <div>
+    <div className='bg-gradient-to-r from-blue-900 to-zinc-900 text-white'>
       <div className='flex justify-between'>
         <Menu />
-        <div className='flex flex-col p-20 w-full h-screen'>
+        <div className='flex flex-col p-20 w-full h-screen '>
           <div className='flex justify-between items-center'>
-            <h1 className='text-2xl font-semibold text-zinc-600 flex items-center gap-4'>New audience <FontAwesomeIcon className='text-zinc-600 text-lg' icon={faWandMagicSparkles} /></h1>
+            <h1 className='text-2xl font-semibold text-zinc-300 flex items-center gap-4'>New audience <FontAwesomeIcon className='text-lg' icon={faWandMagicSparkles} /></h1>
             <button className='bg-blue-600 text-white w-28 h-8 rounded-full font-semibold outline-none'>Save</button>
           </div>
           <div className='flex flex-wrap gap-2 mt-20 '>
-            <div className='w-64 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
+            <div className='w-80 outline-none border-2 border-zinc-300 rounded px-2 py-2 flex gap-2'>
               <p>Name: </p>
-              <input type="text" placeholder='John Doe  ' className='outline-none w-full' />
+              <input type="text" placeholder='John Doe  ' className='outline-none w-full bg-transparent' />
             </div>
-            <div className='w-48 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
+            <div className='w-80 outline-none border-2 border-zinc-300 rounded px-2 py-2 flex gap-2'>
               <p>Gender </p>
               <select className='outline-none w-full'>
-                <option>Male</option>
+                <option>Male</option>   z
                 <option>Female</option>
                 <option>Non-Binary</option>
                 <option>Genderqueer</option>
                 <option>Genderfluid</option>
                 <option>Agender</option>
-                <option>Bigender</option>
+                <option>Bigender</option>  
                 <option>Androgynous</option>
-                <option>Demiboy</option>
+                <option>Demiboy</option> 
                 <option>Demigirl</option>
                 <option>Genderflux</option>
                 <option>Gender Nonconforming</option>
@@ -50,7 +50,7 @@ const App = () => {
                 <option>Prefer not to say</option>
               </select>
             </div>
-            <div className='w-40 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
+            <div className='w-40 outline-none border-2 border-zinc-300 rounded px-2 py-2 flex gap-2'>
               <p>Age: </p>
               <input type="number" placeholder='20' className='outline-none w-full' />
             </div>
@@ -58,19 +58,19 @@ const App = () => {
           <div className='mt-5 flex flex-col'>
             <p className='text-lg font-semibold text-blue-600 flex items-center gap-4'>Academics</p>
             <div className='flex flex-wrap gap-2 mt-1'>
-              <div className='w-80 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
+              <div className='w-80 outline-none border-2 border-zinc-300 rounded px-2 py-2 flex gap-2'>
                 <p>University:</p>
                 <input type="text" placeholder='Jharkhand Rai University' className='outline-none w-full' />
               </div>
-              <div className='w-72 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
+              <div className='w-72 outline-none border-2 border-zinc-300 rounded px-2 py-2 flex gap-2'>
                 <p>Stream:</p>
                 <input type="text" placeholder='Computer Science' className='outline-none w-full' />
               </div>
-              <div className='w-80 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
+              <div className='w-80 outline-none border-2 border-zinc-300 rounded px-2 py-2 flex gap-2'>
                 <p>Course:</p>
                 <input type="text" placeholder='B.Tech' className='outline-none w-full' />
               </div>  
-              <div className='w-44 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
+              <div className='w-44 outline-none border-2 border-zinc-300 rounded px-2 py-2 flex gap-2'>
                 <p>Semester:</p>
                 <input type="number" placeholder='3' className='outline-none w-full' />
               </div> 
