9bc6db5316733efcbae30c07aae5b409a370b118
ankitsensei <main.ankitbhagat@gmail.com>
2024-11-07 17:14:14 +0530

    assigned all variables

--- form/src/App.tsx
+++ form/src/App.tsx
@@ -1,8 +1,17 @@
+import {useState} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faWandMagicSparkles } from '@fortawesome/free-solid-svg-icons'
 
+
 import Menu from './components/Menu'
-const App = () => {
+const App: React.FC = ()   => {
+  const [name, setName] = useState<string>("Ankit")
+  const [gender, setGender] = useState<string>("Male")
+  const [age, setAge] = useState<number>(20)
+  const [university, setUniversity] = useState<string>("Jharkhand Rai University")
+  const [stream, setStream] = useState<string>("Computer Science")
+  const [course, setCourse] = useState<string>("B.Tech")
+  const [semester, setSemester] = useState<number>(3)
   return (
     <div>
       <div className='flex justify-between'>
@@ -15,11 +24,11 @@ const App = () => {
           <div className='flex flex-wrap gap-2 mt-20 '>
             <div className='w-80 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
               <p>Name: </p>
-              <input type="text" placeholder='John Doe' className='outline-none w-full' />
+              <input type="text" placeholder='John Doe' className='outline-none w-full' value={name} />
             </div>
             <div className='w-80 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
               <p>Gender </p>
-              <select className='outline-none w-full'>
+              <select className='outline-none w-full ' value={gender}>
                 <option>Male</option>  
                 <option>Female</option>
                 <option>Non-Binary</option>
@@ -52,7 +61,7 @@ const App = () => {
             </div>
             <div className='w-40 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
               <p>Age: </p>
-              <input type="number" placeholder='20' className='outline-none w-full' />
+              <input type="number" placeholder='20' className='outline-none w-full' value={age} />
             </div>
           </div>
           <div className='mt-5 flex flex-col'>
@@ -60,19 +69,19 @@ const App = () => {
             <div className='flex flex-wrap gap-2 mt-1'>
               <div className='w-80 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
                 <p>University:</p>
-                <input type="text" placeholder='Jharkhand Rai University' className='outline-none w-full' />
+                <input type="text" placeholder='Jharkhand Rai University' className='outline-none w-full' value={university}/>
               </div>
               <div className='w-72 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
                 <p>Stream:</p>
-                <input type="text" placeholder='Computer Science' className='outline-none w-full' />
+                <input type="text" placeholder='Computer Science' className='outline-none w-full' value={stream} />
               </div>
               <div className='w-80 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
                 <p>Course:</p>
-                <input type="text" placeholder='B.Tech' className='outline-none w-full' />
+                <input type="text" placeholder='B.Tech' className='outline-none w-full' value={course} />
               </div>  
               <div className='w-44 outline-none border-2 border-zinc-600 rounded px-2 py-2 flex gap-2'>
                 <p>Semester:</p>
-                <input type="number" placeholder='3' className='outline-none w-full' />
+                <input type="number" placeholder='3' className='outline-none w-full' value={semester} />
               </div> 
             </div>
           </div>
